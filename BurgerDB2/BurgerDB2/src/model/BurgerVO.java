50e9273093d7f9732f2bb279a0e38e6015d6836b
mmm55555 <mayfifthfive555@gmail.com>
2024-05-21 17:47:36 +0900

    BurgerDB2 기본 커밋

--- BurgerDB2/BurgerDB2/src/model/BurgerVO.java
+++ BurgerDB2/BurgerDB2/src/model/BurgerVO.java
@@ -1,91 +1,161 @@
 package model;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Objects;
 
 public class BurgerVO implements Serializable, Comparable<BurgerVO> {
-    private int code;
-    private String name;
-    private int price;
-    private String info;
-    public BurgerVO() {
-        super();
-    }
+    private int burgerCode;
+    private String burgerName;
+    private int burgerPrice;
+    private String burgerDescription;
+    private String burgerCategory;
+    private int burgerStock;
+    private Date burgerCreatedAt;
+    private Date burgerUpdatedAt;
+    private int burgerCalories;
+    private String burgerAllergy;
 
-    public BurgerVO(int code, String name, int price, String info) {
-        this.code = code;
-        this.name = name;
-        this.info = info;
-        this.price = price;
+    public BurgerVO(int burgerCode, String burgerName, int burgerPrice, String burgerDescription, String burgerCategory, int burgerStock, Date burgerCreatedAt, Date burgerUpdatedAt, int burgerCalories, String burgerAllergy) {
+        this.burgerCode = burgerCode;
+        this.burgerName = burgerName;
+        this.burgerPrice = burgerPrice;
+        this.burgerDescription = burgerDescription;
+        this.burgerCategory = burgerCategory;
+        this.burgerStock = burgerStock;
+        this.burgerCreatedAt = burgerCreatedAt;
+        this.burgerUpdatedAt = burgerUpdatedAt;
+        this.burgerCalories = burgerCalories;
+        this.burgerAllergy = burgerAllergy;
     }
     public void dan()   {
-        setName(name+" 단품");
+
+        setBurgerName(getBurgerName()+" 단품");
     }
     public void sizeUp()  {
-        price = price + 2000;
-        setName(name+" 사이즈업(+2000)");
+        setBurgerPrice(getBurgerPrice()+2000);
+        setBurgerName(getBurgerName()+" 사이즈업(+2000)");
     }
     public void set() {
-        price = (int)(price*1.3);
-        setName(name+" 세트");
+        setBurgerPrice((int)(getBurgerPrice()*1.3));
+        setBurgerName(getBurgerName()+" 세트");
     }
     public void sizeUpNSet()  {
-        price = (int)(price*1.3) + 2000;
-        setName(name+" 사이즈업+세트");
+        setBurgerPrice((int)(getBurgerPrice()*1.3) + 2000);
+        setBurgerName(getBurgerName()+" 사이즈업+세트");
     }
 
-    public int getCode() {
-        return code;
+    public int getBurgerCode() {
+        return burgerCode;
     }
 
-    public void setCode(int code) {
-        this.code = code;
+    public void setBurgerCode(int burgerCode) {
+        this.burgerCode = burgerCode;
     }
 
-    public String getName() {
-        return name;
+    public String getBurgerName() {
+        return burgerName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setBurgerName(String burgerName) {
+        this.burgerName = burgerName;
     }
 
-    public int getPrice() {
-        return price;
+    public int getBurgerPrice() {
+        return burgerPrice;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
+    public void setBurgerPrice(int burgerPrice) {
+        this.burgerPrice = burgerPrice;
     }
 
-    public String getInfo() {
-        return info;
+    public String getBurgerDescription() {
+        return burgerDescription;
     }
 
-    public void setInfo(String info) {
-        this.info = info;
+    public void setBurgerDescription(String burgerDescription) {
+        this.burgerDescription = burgerDescription;
+    }
+
+    public String getBurgerCategory() {
+        return burgerCategory;
+    }
+
+    public void setBurgerCategory(String burgerCategory) {
+        this.burgerCategory = burgerCategory;
+    }
+
+    public int getBurgerStock() {
+        return burgerStock;
+    }
+
+    public void setBurgerStock(int burgerStock) {
+        this.burgerStock = burgerStock;
+    }
+
+    public Date getBurgerCreatedAt() {
+        return burgerCreatedAt;
+    }
+
+    public void setBurgerCreatedAt(Date burgerCreatedAt) {
+        this.burgerCreatedAt = burgerCreatedAt;
+    }
+
+    public Date getBurgerUpdatedAt() {
+        return burgerUpdatedAt;
+    }
+
+    public void setBurgerUpdatedAt(Date burgerUpdatedAt) {
+        this.burgerUpdatedAt = burgerUpdatedAt;
+    }
+
+    public int getBurgerCalories() {
+        return burgerCalories;
+    }
+
+    public void setBurgerCalories(int burgerCalories) {
+        this.burgerCalories = burgerCalories;
+    }
+
+    public String getBurgerAllergy() {
+        return burgerAllergy;
+    }
+
+    public void setBurgerAllergy(String burgerAllergy) {
+        this.burgerAllergy = burgerAllergy;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        BurgerVO burger = (BurgerVO) o;
-        return code == burger.code && price == burger.price && Objects.equals(name, burger.name) && Objects.equals(info, burger.info);
+        BurgerVO burgerVO = (BurgerVO) o;
+        return getBurgerCode() == burgerVO.getBurgerCode() && getBurgerPrice() == burgerVO.getBurgerPrice() && getBurgerStock() == burgerVO.getBurgerStock() && getBurgerCalories() == burgerVO.getBurgerCalories() && Objects.equals(getBurgerName(), burgerVO.getBurgerName()) && Objects.equals(getBurgerDescription(), burgerVO.getBurgerDescription()) && Objects.equals(getBurgerCategory(), burgerVO.getBurgerCategory()) && Objects.equals(getBurgerCreatedAt(), burgerVO.getBurgerCreatedAt()) && Objects.equals(getBurgerUpdatedAt(), burgerVO.getBurgerUpdatedAt()) && Objects.equals(getBurgerAllergy(), burgerVO.getBurgerAllergy());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(code, name, price, info);
+        return Objects.hash(getBurgerCode(), getBurgerName(), getBurgerPrice(), getBurgerDescription(), getBurgerCategory(), getBurgerStock(), getBurgerCreatedAt(), getBurgerUpdatedAt(), getBurgerCalories(), getBurgerAllergy());
     }
 
     @Override
     public String toString() {
-        return getCode()+"번\t\t"+getName()+"\t"+getPrice()+"원\t"+getInfo();
+        return "BurgerVO{" +
+                "burgerCode=" + burgerCode +
+                ", burgerName='" + burgerName + '\'' +
+                ", burgerPrice=" + burgerPrice +
+                ", burgerDescription='" + burgerDescription + '\'' +
+                ", burgerCategory='" + burgerCategory + '\'' +
+                ", burgerStock=" + burgerStock +
+                ", burgerCreatedAt=" + burgerCreatedAt +
+                ", burgerUpdatedAt=" + burgerUpdatedAt +
+                ", burgerCalories=" + burgerCalories +
+                ", burgerAllergy='" + burgerAllergy + '\'' +
+                '}';
     }
 
     @Override
     public int compareTo(BurgerVO o) {
-        return getCode()-o.getCode();
+        return 0;
     }
 }
--- BurgerDB2/BurgerDB2/src/model/SideVO.java
+++ BurgerDB2/BurgerDB2/src/model/SideVO.java
@@ -1,64 +1,145 @@
 package model;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.Objects;
 
 public class SideVO implements Serializable, Comparable<SideVO> {
-    private int code;
-    private String name;
-    private int price;
-    public SideVO()   {
-        super();
-    }
-    public SideVO(int code, String sdName, int price) {
-        this.code = code;
-        this.name = sdName;
-        this.price = price;
+    private int sideCode;
+    private String sideName;
+    private int sidePrice;
+    private String sideDescription;
+    private String category;
+    private int sideStock;
+    private Date sideCreatedAt;
+    private Date sideUpdatedAt;
+    private int sideCalories;
+    private String sideAllergyInfo;
+
+    public SideVO(int sideCode, String sideName, int sidePrice, String sideDescription, String category, int sideStock, Date sideCreatedAt, Date sideUpdatedAt, int sideCalories, String sideAllergyInfo) {
+        this.sideCode = sideCode;
+        this.sideName = sideName;
+        this.sidePrice = sidePrice;
+        this.sideDescription = sideDescription;
+        this.category = category;
+        this.sideStock = sideStock;
+        this.sideCreatedAt = sideCreatedAt;
+        this.sideUpdatedAt = sideUpdatedAt;
+        this.sideCalories = sideCalories;
+        this.sideAllergyInfo = sideAllergyInfo;
     }
 
-    public int getCode() {
-        return code;
+    public int getSideCode() {
+        return sideCode;
     }
 
-    public void setCode(int code) {
-        this.code = code;
+    public void setSideCode(int sideCode) {
+        this.sideCode = sideCode;
     }
 
-    public String getName() {
-        return name;
+    public String getSideName() {
+        return sideName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setSideName(String sideName) {
+        this.sideName = sideName;
     }
 
-    public int getPrice() {
-        return price;
+    public int getSidePrice() {
+        return sidePrice;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
+    public void setSidePrice(int sidePrice) {
+        this.sidePrice = sidePrice;
+    }
+
+    public String getSideDescription() {
+        return sideDescription;
+    }
+
+    public void setSideDescription(String sideDescription) {
+        this.sideDescription = sideDescription;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getSideStock() {
+        return sideStock;
+    }
+
+    public void setSideStock(int sideStock) {
+        this.sideStock = sideStock;
+    }
+
+    public Date getSideCreatedAt() {
+        return sideCreatedAt;
+    }
+
+    public void setSideCreatedAt(Date sideCreatedAt) {
+        this.sideCreatedAt = sideCreatedAt;
+    }
+
+    public Date getSideUpdatedAt() {
+        return sideUpdatedAt;
+    }
+
+    public void setSideUpdatedAt(Date sideUpdatedAt) {
+        this.sideUpdatedAt = sideUpdatedAt;
+    }
+
+    public int getSideCalories() {
+        return sideCalories;
+    }
+
+    public void setSideCalories(int sideCalories) {
+        this.sideCalories = sideCalories;
+    }
+
+    public String getSideAllergyInfo() {
+        return sideAllergyInfo;
+    }
+
+    public void setSideAllergyInfo(String sideAllergyInfo) {
+        this.sideAllergyInfo = sideAllergyInfo;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        SideVO side = (SideVO) o;
-        return code == side.code && price == side.price && Objects.equals(name, side.name);
+        SideVO sideVO = (SideVO) o;
+        return getSideCode() == sideVO.getSideCode() && getSidePrice() == sideVO.getSidePrice() && getSideStock() == sideVO.getSideStock() && getSideCalories() == sideVO.getSideCalories() && Objects.equals(getSideName(), sideVO.getSideName()) && Objects.equals(getSideDescription(), sideVO.getSideDescription()) && Objects.equals(getCategory(), sideVO.getCategory()) && Objects.equals(getSideCreatedAt(), sideVO.getSideCreatedAt()) && Objects.equals(getSideUpdatedAt(), sideVO.getSideUpdatedAt()) && Objects.equals(getSideAllergyInfo(), sideVO.getSideAllergyInfo());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(code, name, price);
+        return Objects.hash(getSideCode(), getSideName(), getSidePrice(), getSideDescription(), getCategory(), getSideStock(), getSideCreatedAt(), getSideUpdatedAt(), getSideCalories(), getSideAllergyInfo());
     }
 
     @Override
     public String toString() {
-        return getCode()+"번\t\t"+getName()+"\t"+getPrice()+"원";
+        return "SideVO{" +
+                "sideCode=" + sideCode +
+                ", sideName='" + sideName + '\'' +
+                ", sidePrice=" + sidePrice +
+                ", sideDescription='" + sideDescription + '\'' +
+                ", category='" + category + '\'' +
+                ", sideStock=" + sideStock +
+                ", sideCreatedAt=" + sideCreatedAt +
+                ", sideUpdatedAt=" + sideUpdatedAt +
+                ", sideCalories=" + sideCalories +
+                ", sideAllergyInfo='" + sideAllergyInfo + '\'' +
+                '}';
     }
+
     @Override
     public int compareTo(SideVO o) {
-        return getCode() - o.getCode();
+        return 0;
     }
 }
